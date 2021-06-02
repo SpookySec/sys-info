872f73f9ce68add7e518dd9ca7569e2763e330c4
Spooky Sec <anonymousbot100110@gmail.com>
2021-06-02 21:23:49 +0300

    cooooooomit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 extern crate sys_info;
-use std::{mem, process::exit};
+use std::process::exit;
 
-use sys_info::{linux_os_release, mem_info, LinuxOSReleaseInfo, MemInfo};
-
+use sys_info::{DiskInfo, LinuxOSReleaseInfo, MemInfo, disk_info, hostname, linux_os_release, mem_info, os_release};
+use chrono::Utc;
 fn main() {
+    //// HOST INFORMATION
+    println!("\n\n-=-= HOST INFORMATION =-=-");
+    println!("Hostname: {}", hostname().unwrap_or_default());
+    println!("Date: {}", Utc::now());
+
+
     //// OS RELEASE INFORMATION
+    println!("\n\n-=-= RELEASE INFORMATION =-=-");
     let release_info: LinuxOSReleaseInfo = match linux_os_release() {
         Err(e) => {
             eprintln!("Error: {}", e);
@@ -13,14 +20,13 @@ fn main() {
         Ok(release_info) => release_info,
     };
 
-    println!("Os Release: {} {} release {} {}", &release_info.pretty_name.unwrap_or_default(),
-        &release_info.build_id.unwrap_or_default(),
-        &release_info.version.unwrap_or_default(),
-        &release_info.cpe_name.unwrap_or_default()
+    println!("{}: {}", release_info.pretty_name.unwrap_or_default(),
+        os_release().unwrap_or_default()
     );
 
 
     //// LINUX MEMORY INFORMATION
+    println!("\n\n-=-= MEMORY INFORMATION =-=-");
     let mem_info: MemInfo = match mem_info() {
         Err(e) => {
             eprintln!("Error: {}", e);
@@ -34,4 +40,17 @@ fn main() {
         &mem_info.avail,
         &mem_info.swap_total
     );
+
+
+    //// DISK INFORMATION
+    println!("\n\n-=-= DISK INFORMATION =-=-");
+    let disk_info: DiskInfo = match disk_info() {
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            exit(1)
+        }
+        Ok(disk_info) => disk_info
+    };
+    
+    println!("Total Disk Space: {} bytes\nFree Disk Space: {} bytes", &disk_info.total, &disk_info.free)
 }
